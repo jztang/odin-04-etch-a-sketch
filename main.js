a05b5e6e88daed43e0a63dba2ae0206fbdf0864d
jztang <12689662+jztang@users.noreply.github.com>
2023-03-26 21:24:41 -0400

    Change square color on hover

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ const container = document.querySelector(".container");
 for (let i = 1; i <= 256; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseenter", () => {
+    square.style["background-color"] = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  });
   container.appendChild(square);
 }
