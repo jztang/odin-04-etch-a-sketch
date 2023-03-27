b2b82d7c58f9e42510df08863a5b1b4ab01ae859
jztang <12689662+jztang@users.noreply.github.com>
2023-03-27 11:05:39 -0400

    Add button to generate a new grid

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const container = document.querySelector(".container");
+function generateGrid(size) {
+  const container = document.querySelector(".container");
+  container.replaceChildren();
 
-for (let i = 1; i <= 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("mouseenter", () => {
-    square.style["background-color"] = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  });
-  container.appendChild(square);
+  for (let i = 1; i <= size * size; i++) {
+    const square = document.createElement("div");
+    square.style.height = `${800 / size}px`;
+    square.style.width = `${800 / size}px`;
+    square.addEventListener("mouseenter", () => {
+      square.style["background-color"] = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    });
+    container.appendChild(square);
+  }
 }
+
+document.querySelector(".new-grid-btn").addEventListener("click", () => {
+  const newSize = prompt("Enter new grid size (max 100)", 64);
+  generateGrid(newSize);
+});
+
+generateGrid(16);
